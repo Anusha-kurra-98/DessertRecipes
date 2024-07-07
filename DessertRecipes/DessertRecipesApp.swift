ab55411dd205be242bc2088d908c44248383418a
Anusha Kurra <161259663+Anusha-kurra-98@users.noreply.github.com>
2024-07-07 14:15:45 -0500

    Initial Commit

---
+++ DessertRecipes/DessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesApp.swift
+//  DessertRecipes
+//
+//  Created by anusha  kurra on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
